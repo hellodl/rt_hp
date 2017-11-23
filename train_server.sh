b7794dfedf95bc0cc50b0b68c0749ae62b2d38ab
g.hy <hanyang.gui@gmail.com>
2017-11-24 03:02:06 +0800

    add train_server.sh & valid_server.sh

---
+++ train_server.sh
@@ -0,0 +1,3 @@
+./rmpe_dataset_server-master/build/rmpe_dataset_server \
+../../input/AIC/train_dataset.h5 5555
+
---
+++ valid_server.sh
@@ -0,0 +1,2 @@
+./rmpe_dataset_server-master/build/rmpe_dataset_server \
+../../input/AIC/valid_dataset.h5 5556
